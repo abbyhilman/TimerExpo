3a2112f5c259ee5ee178b969b6d3bdb482fe5201
Abby Hilman <abbyhilman@gmail.ccom>
2021-03-30 09:55:51 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Platform, SafeAreaView } from "react-native";
+import { Focus } from "./src/features/focus/Focus";
+import { Timer } from "./src/features/timer/Timer";
+import { colors } from "./src/utils/colors";
+import { spacing } from "./src/utils/sizes";
 
 export default function App() {
+  const [focusSubject, setFocusSubject] = useState('gardening');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {focusSubject ? (
+        <Timer focusSubject={focusSubject}/>
+      ) : (
+        <Focus addSubject={setFocusSubject} />
+      )}
+      <Text>{focusSubject}</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS == 'ios' ? spacing.md : spacing.lg,
+    backgroundColor: colors.darkBlue,
   },
 });
